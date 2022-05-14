924fe03a553efee251e1817b6e987eea0e531456
nikolay <tneomichi@gmail.com>
2022-05-14 15:54:12 +0500

    fix console

---
+++ LearnGrpc.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using Greet;
+using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearnGrpc.ConsoleApp
+{
+    class Program
+    {
+        async static  Task Main(string[] args)
+        {
+            await StreamingTest();
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+          
+        }
+
+       async static Task StreamingTest()
+        {    
+  
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+      
+             var client = new Greeter.GreeterClient(channel);
+
+             var call = client.SayHellos(new HelloRequest { Name = "Клиент" });
+
+            while (await call.ResponseStream.MoveNext())
+            {
+                Console.WriteLine("Greeting: " + call.ResponseStream.Current.Message);
+                
+            }
+
+          
+        }
+                    
+
+    }
+}
