e11eb05b4d667c16b93a7a6aca85a57033314155
nikolay <tneomichi@gmail.com>
2022-06-02 10:13:23 +0500

    fix string

--- LearnGrpc.Server/Startup.cs
+++ LearnGrpc.Server/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using LearnGrpc.Server.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -47,7 +48,7 @@ namespace LearnGrpc.Server
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<GreeterService>().EnableGrpcWeb();
-
+                endpoints.MapGrpcService<NoteService>().EnableGrpcWeb();
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
