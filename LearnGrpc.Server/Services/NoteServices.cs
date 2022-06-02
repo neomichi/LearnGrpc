568f35e0ed5c14e2c268c8fce3b8feb20239a84a
nikolay <tneomichi@gmail.com>
2022-06-02 10:07:28 +0500

    сервис notes

---
+++ LearnGrpc.Server/Services/NoteServices.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using Notes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnGrpc.Server.Services
+{
+    public class NoteService:Notes.NoteService.NoteServiceBase
+    {
+
+        public override Task<Note> AddNote(AddNoteRequest request, ServerCallContext context)
+        {
+            return base.AddNote(request, context);
+        }
+
+        public override Task<GetNotesResponse> GetNotes(GetNotesRequest request, ServerCallContext context)
+        {
+            return base.GetNotes(request, context);
+        }
+
+        public override Task<Note> DeleteNote(DeleteNoteRequest request, ServerCallContext context)
+        {
+            return base.DeleteNote(request, context);
+        }
+    }
+}
